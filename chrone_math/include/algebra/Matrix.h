36043aac76f3a03b04ba76e8b0363c18aa51b076
Yoann NOAIL <y.noail@student.isartdigital.com>
2018-11-11 19:55:32 +0100

    removing and replacing complex vector/matrix code

--- chrone_math/include/algebra/Matrix.h
+++ chrone_math/include/algebra/Matrix.h
@@ -1,193 +1,111 @@
 #pragma once
 
 #include "NativeType.h"
-#include "Vector.h"
-#include "MatrixComponent.h"
 
 namespace chrone::math
 {
 
-template<class T>
-struct Matrix3x3
+template<
+	class T, 
+	Uint32 ColumnCount, 
+	Uint32 RowCount>
+struct Matrix
 {
-	using TypeComponentStruct = typename T::TypeComponentStruct;
-	using TypeColumnStruct = typename T::TypeColumnStruct;
+	constexpr static CompCount = ColumnCount* RowCount;
 
-	using TypeVector3D = typename T::TypeVector3D;
-	using TypeVector3DRef = std::add_lvalue_reference_t<TypeVector3D>;
-	using TypeVector3DCRef = std::add_lvalue_reference_t<TypeVector3D const>;
-	using TypeVector3DRRef = std::add_rvalue_reference_t<TypeVector3D const>;
+	inline Matrix() = default;
+	inline Matrix(const Matrix&) = default;
+	inline Matrix(Matrix&&) = default;
+	~Matrix() = default;
 
-
-	inline Matrix3x3() = default;
-	inline Matrix3x3(Matrix3x3 const&) = default;
-	inline Matrix3x3(Matrix3x3&&) = default;
-	inline ~Matrix3x3() = default;
-
-	inline Matrix3x3(TypeVector3DRef col0, TypeVector3DRef col1, TypeVector3DRef col2);
-
-	inline Matrix3x3& operator=(Matrix3x3 const&) = default;
-	inline Matrix3x3& operator=(Matrix3x3&&) = default;
-
-	union
-	{
-		Vector1DComponent<TypeComponentStruct, 0>	m00;
-		Vector1DComponent<TypeComponentStruct, 1>	m10;
-		Vector1DComponent<TypeComponentStruct, 2>	m20;
-
-		Vector1DComponent<TypeComponentStruct, 3>	m01;
-		Vector1DComponent<TypeComponentStruct, 4>	m11;
-		Vector1DComponent<TypeComponentStruct, 5>	m21;
-
-		Vector1DComponent<TypeComponentStruct, 6>	m02;
-		Vector1DComponent<TypeComponentStruct, 7>	m12;
-		Vector1DComponent<TypeComponentStruct, 8>	m22;
-
-		Vector3DComponent<TypeColumnStruct, 0, 1, 2> c0;
-		Vector3DComponent<TypeColumnStruct, 3, 4, 5> c1;
-		Vector3DComponent<TypeColumnStruct, 6, 7, 8> c2;
-
-		Vector3DComponent<TypeColumnStruct, 0, 3, 6> r0;
-		Vector3DComponent<TypeColumnStruct, 1, 4, 7> r1;
-		Vector3DComponent<TypeColumnStruct, 2, 5, 8> r2;
-
-		Matrix3x3Component<T, 0, 1, 2, 3, 4, 5, 6, 7, 8>	matrix;
-		Matrix3x3Component<T, 0, 3, 6, 1, 4, 7, 2, 5, 8>	transpose;
-	};
+	T	data[CompCount];
 };
 
 
 template<class T>
-struct Matrix3x3MapData
+struct Matrix<T, 3, 3>
 {
-	using Type = T;
-	using TypeComponentStruct = Vector2DMapData<T>;
-	using TypeColumnStruct = Vector3DMapData<T>;
+	constexpr static ColumnCount = 3;
+	constexpr static RowCount = 3;
+	constexpr static CompCount = ColumnCount* RowCount;
+	inline Matrix() = default;
+	inline Matrix(const Matrix&) = default;
+	inline Matrix(Matrix&&) = default;
+	~Matrix() = default;
 
-	using TypeVector3D = Vector3D<Vector3DMapData<T>>;
+	inline explicit Matrix(const T* srcData);
+	inline explicit Matrix(const T* col0, const T* col1, const T* col2);
 
-	using TypeMatrix3x3 = Matrix3x3<Matrix3x3MapData>;
+
+	T	data[CompCount];
 };
 
-using Matrix3x3MapDataDec32 = Matrix3x3MapData<Decimal32>;
-using Matrix3x3MapDataDec64 = Matrix3x3MapData<Decimal64>;
-using Matrix3x3Dec32 = Matrix3x3<Matrix3x3MapDataDec32>;
-using Matrix3x3Dec64 = Matrix3x3<Matrix3x3MapDataDec64>;
+
+template<class T>
+struct Matrix<T, 4, 4>
+{
+	constexpr static ColumnCount = 4;
+	constexpr static RowCount = 4;
+	constexpr static CompCount = ColumnCount* RowCount;
+
+	inline Matrix() = default;
+	inline Matrix(const Matrix&) = default;
+	inline Matrix(Matrix&&) = default;
+	~Matrix() = default;
+
+	inline explicit Matrix(const T* srcData);
+	inline explicit Matrix(const T* col0, const T* col1, const T* col2, const T* col3);
+
+
+	T	data[CompCount];
+};
+
 
 
 template<class T>
 inline 
-Matrix3x3<T>::Matrix3x3(
-	TypeVector3DRef col0, 
-	TypeVector3DRef col1, 
-	TypeVector3DRef col2):
-	matrix{ col0, col1, col2 }
+Matrix<T, 3, 3>::Matrix(
+	const T* srcData)
 {
+	std::memcpy(data, srcData, sizeof(T) * CompCount);
 }
-
-
-
-
-template<class T>
-struct Matrix4x4
-{
-	using TypeComponentStruct = typename T::TypeComponentStruct;
-	using TypeColumn3DStruct = typename T::TypeColumn3DStruct;
-	using TypeColumn4DStruct = typename T::TypeColumn4DStruct;
-	using TypeMatrix3x3Struct = typename T::TypeMatrix3x3Struct;
-	//using Vector4DTypeStruct = typename T::Type4D;
-
-	using TypeVector3D = typename T::TypeVector3D;
-	using TypeVector3DRef = std::add_lvalue_reference_t<TypeVector3D>;
-	using TypeVector3DCRef = std::add_lvalue_reference_t<TypeVector3D const>;
-	using TypeVector3DRRef = std::add_rvalue_reference_t<TypeVector3D const>;
-
-	using TypeVector4D = typename T::TypeVector4D;
-	using TypeVector4DRef = std::add_lvalue_reference_t<TypeVector4D>;
-	using TypeVector4DCRef = std::add_lvalue_reference_t<TypeVector4D const>;
-	using TypeVector4DRRef = std::add_rvalue_reference_t<TypeVector4D const>;
-
-	inline Matrix4x4() = default;
-	inline Matrix4x4(Matrix4x4 const&) = default;
-	inline Matrix4x4(Matrix4x4&&) = default;
-	inline ~Matrix4x4() = default;
-
-	inline Matrix4x4(TypeVector4DCRef col0, TypeVector4DCRef col1, 
-		TypeVector4DCRef col2, TypeVector4DCRef col3);
-
-	inline Matrix4x4& operator=(Matrix4x4 const&) = default;
-	inline Matrix4x4& operator=(Matrix4x4&&) = default;
-
-	union
-	{
-		Vector1DComponent<TypeComponentStruct, 0>	m00;
-		Vector1DComponent<TypeComponentStruct, 1>	m10;
-		Vector1DComponent<TypeComponentStruct, 2>	m20;
-		Vector1DComponent<TypeComponentStruct, 3>	m30;
-		
-		Vector1DComponent<TypeComponentStruct, 4>	m01;
-		Vector1DComponent<TypeComponentStruct, 5>	m11;
-		Vector1DComponent<TypeComponentStruct, 6>	m21;
-		Vector1DComponent<TypeComponentStruct, 7>	m31;
-
-		Vector1DComponent<TypeComponentStruct, 8>	m02;
-		Vector1DComponent<TypeComponentStruct, 9>	m12;
-		Vector1DComponent<TypeComponentStruct, 10>	m22;
-		Vector1DComponent<TypeComponentStruct, 11>	m32;
-
-		Vector1DComponent<TypeComponentStruct, 12>	m03;
-		Vector1DComponent<TypeComponentStruct, 13>	m13;
-		Vector1DComponent<TypeComponentStruct, 14>	m23;
-		Vector1DComponent<TypeComponentStruct, 15>	m33;
-
-		Vector4DComponent<TypeColumn4DStruct, 0, 1, 2, 3> c0;
-		Vector4DComponent<TypeColumn4DStruct, 4, 5, 6, 7> c1;
-		Vector4DComponent<TypeColumn4DStruct, 8, 9, 10, 11> c2;
-		Vector4DComponent<TypeColumn4DStruct, 12, 13, 14, 15> c3;
-
-		Vector4DComponent<TypeColumn4DStruct, 0, 4, 8, 12> r0;
-		Vector4DComponent<TypeColumn4DStruct, 1, 5, 9, 13> r1;
-		Vector4DComponent<TypeColumn4DStruct, 2, 6, 10, 14> r2;
-		Vector4DComponent<TypeColumn4DStruct, 3, 7, 11, 15> r3;
-
-		Matrix3x3Component<TypeMatrix3x3Struct, 0, 1, 2, 4, 5, 6, 8, 9, 10>	mat3x3;
-		Matrix3x3Component<TypeMatrix3x3Struct, 0, 4, 8, 1, 5, 9, 2, 6, 10>	mat3x3Transpose;
-
-		Matrix4x4Component<T, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15> matrix;
-		Matrix4x4Component<T, 0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15>	transpose;
-	};
-};
 
 
 template<class T>
 inline 
-Matrix4x4<T>::Matrix4x4(
-	TypeVector4DCRef col0, TypeVector4DCRef col1, 
-	TypeVector4DCRef col2, TypeVector4DCRef col3):
-	matrix{ col0, col1, col2, col3 }
+Matrix<T, 3, 3>::Matrix(
+	const T* col0, 
+	const T* col1, 
+	const T* col2)
 {
+	std::memcpy(data, col0, sizeof(T) * RowCount);
+	std::memcpy(data + 3, col1, sizeof(T) * RowCount);
+	std::memcpy(data + 6, col2, sizeof(T) * RowCount);
 }
 
 
 template<class T>
-struct Matrix4x4MapData
+inline 
+Matrix<T, 4, 4>::Matrix(
+	const T* srcData)
 {
-	using Type = T;
-	using Type4D = Vector4DMapData<T>;
-	using TypeComponentStruct = Vector2DMapData<T>;
-	using TypeColumn3DStruct = Vector3DMapData<T>;
-	using TypeColumn4DStruct = Type4D;
+	std::memcpy(data, srcData, sizeof(T) * CompCount);
+}
 
-	using TypeVector3D = Vector3D<TypeColumn3DStruct>;
-	using TypeVector4D = Vector4D<TypeColumn4DStruct>;
 
-	using TypeMatrix3x3Struct = Matrix3x3MapData<T>;
-	using TypeMatrix4x4 = Matrix4x4<Matrix4x4MapData>;
-};
+template<class T>
+inline 
+Matrix<T, 4, 4>::Matrix(
+	const T* col0, 
+	const T* col1, 
+	const T* col2, 
+	const T* col3)
+{
+	std::memcpy(data, col0, sizeof(T) * RowCount);
+	std::memcpy(data + 4, col1, sizeof(T) * RowCount);
+	std::memcpy(data + 8, col2, sizeof(T) * RowCount);
+	std::memcpy(data + 12, col3, sizeof(T) * RowCount);
+}
 
-using Matrix4x4MapDataDec32 = Matrix4x4MapData<Decimal32>;
-using Matrix4x4MapDataDec64 = Matrix4x4MapData<Decimal64>;
-using Matrix4x4Dec32 = Matrix4x4<Matrix4x4MapDataDec32>;
-using Matrix4x4Dec64 = Matrix4x4<Matrix4x4MapDataDec64>;
 
 }
--- chrone_math/include/algebra/Vector.h
+++ chrone_math/include/algebra/Vector.h
@@ -1,291 +1,137 @@
 #pragma once
 
 #include "NativeType.h"
-#include "VectorComponent.h"
 
 namespace chrone::math
 {
 
-template<class T>
-struct Vector2D
+template<class T, Uint32 ComponentCount>
+struct Vector
 {
-	using ComponentType = typename T::Type;
+	inline Vector() = default;
+	inline Vector(const Vector&) = default;
+	inline Vector(Vector&&) = default;
+	~Vector() = default;
 
-	inline Vector2D() = default;
-	inline Vector2D(Vector2D const&) = default;
-	inline Vector2D(Vector2D&&) = default;
-	inline ~Vector2D() = default;
-
-	inline explicit Vector2D(ComponentType const value0, ComponentType const value1);
-	inline explicit Vector2D(ComponentType const value);
-
-	inline Vector2D& operator=(Vector2D const&) = default;
-	inline Vector2D& operator=(Vector2D&&) = default;
-
-	union
-	{
-		Vector1DComponent<T, 0> x;
-		Vector1DComponent<T, 1>	y;
-
-		Vector2DComponent<T, 0, 1>	xy{};
-		Vector2DComponent<T, 1, 0>	yx;
-	};
+	T	data[ComponentCount];
 };
 
 
 template<class T>
-inline 
-Vector2D<T>::Vector2D(
-	ComponentType const value0, 
-	ComponentType const value1):
-	xy{value0, value1}
-{
-}
-
-
-template<class T>
-inline 
-Vector2D<T>::Vector2D(ComponentType const value):
-	xy{value, value}
-{
-}
-
-
-template<class T>
-Vector2D<T>	operator+(
-	Vector2D<T> const& l,
-	const Vector2D<T>& r)
-{
-	return Vector2D<T>{l.x + r.x, l.y + r.y};
-}
-
-
-template<class T>
-Vector2D<T>	operator-(
-	Vector2D<T> const& l,
-	const Vector2D<T>& r)
-{
-	return Vector2D<T>{l.x - r.x, l.y - r.y};
-}
-
-
-template<class T>
-struct Vector2DMapData
+struct Vector<T, 2>
 {
 	using Type = T;
-	using Type2D = Vector2D<Vector2DMapData>;
-};
+	constexpr static CompCount = 2;
 
+	inline Vector() = default;
+	inline Vector(const Vector&) = default;
+	inline Vector(Vector&&) = default;
+	~Vector() = default;
 
-using Vector2DMapDataDec32 = Vector2DMapData<Decimal32>;
-using Vector2DMapDataDec64 = Vector2DMapData<Decimal64>;
-using Vector2DDec32 = Vector2D<Vector2DMapDataDec32>;
-using Vector2DDec64 = Vector2D<Vector2DMapDataDec64>;
+	inline explicit Vector(const T x, const T y);
+	inline explicit Vector(const T* srcData);
 
-template<class T>
-struct Vector3D
-{
-	using ComponentType = typename T::Type;
-
-	using TypeVector2DMapData = Vector2DMapData<ComponentType>;
-
-	inline Vector3D() = default;
-	inline Vector3D(Vector3D const&) = default;
-	inline Vector3D(Vector3D &&) = default;
-	inline ~Vector3D() = default;
-
-	inline explicit Vector3D(ComponentType const value0, ComponentType const value1, ComponentType const value2);
-	inline explicit Vector3D(ComponentType const value);
-
-	inline Vector3D& operator=(Vector3D const&) = default;
-	inline Vector3D& operator=(Vector3D &&) = default;
-
-	union
-	{
-		Vector1DComponent<T, 0> x;
-		Vector1DComponent<T, 1>	y;
-		Vector1DComponent<T, 2>	z;
-
-		Vector2DComponent<TypeVector2DMapData, 0, 1>	xy;
-		Vector2DComponent<TypeVector2DMapData, 1, 0>	yx;
-		Vector2DComponent<TypeVector2DMapData, 0, 2>	xz;
-		Vector2DComponent<TypeVector2DMapData, 2, 0>	zx;
-		Vector2DComponent<TypeVector2DMapData, 1, 2>	yz;
-		Vector2DComponent<TypeVector2DMapData, 2, 1>	zy;
-
-		Vector3DComponent<T, 0, 1, 2> xyz;
-		Vector3DComponent<T, 0, 2, 1> xzy;
-		Vector3DComponent<T, 1, 0, 2> yxz;
-		Vector3DComponent<T, 1, 2, 0> yzx;
-		Vector3DComponent<T, 2, 0, 1> zxy;
-		Vector3DComponent<T, 2, 1, 0> zyx;
-	};
+	T	data[CompCount];
 };
 
 
 template<class T>
-inline Vector3D<T>::Vector3D(
-	ComponentType const value0,
-	ComponentType const value1,
-	ComponentType const value2) :
-	xyz{ value0, value1, value2 }
-{
-}
-
-
-template<class T>
-inline Vector3D<T>::Vector3D(ComponentType const value) :
-	xyz{ value, value, value }
-{
-}
-
-
-template<class T>
-Vector3D<T>	operator+(
-	Vector3D<T> const& l,
-	const Vector3D<T>& r)
-{
-	return Vector3D<T>{l.x + r.x, l.y + r.y, l.z + r.z};
-}
-
-
-template<class T>
-Vector3D<T>	operator-(
-	Vector3D<T> const& l,
-	const Vector3D<T>& r)
-{
-	return Vector3D<T>{l.x - r.x, l.y - r.y, l.z - r.z};
-}
-
-template<class T>
-struct Vector3DMapData
+struct Vector<T, 3>
 {
 	using Type = T;
-	using Type2D = Vector2D<Vector2DMapData<T>>;
-	using Type3D = Vector3D<Vector3DMapData>;
-};
+	constexpr static CompCount = 3;
 
+	inline Vector() = default;
+	inline Vector(const Vector&) = default;
+	inline Vector(Vector&&) = default;
+	~Vector() = default;
 
-using Vector3DMapDataDec32 = Vector3DMapData<Decimal32>;
-using Vector3DMapDataDec64 = Vector3DMapData<Decimal64>;
-using Vector3DDec32 = Vector3D<Vector3DMapDataDec32>;
-using Vector3DDec64 = Vector3D<Vector3DMapDataDec64>;
+	inline explicit Vector(const T x, const T y, const T z);
+	inline explicit Vector(const T* srcData);
 
-
-
-
-template<class T>
-struct Vector4D
-{
-	using ComponentType = typename T::Type;
-	using TypeVector3D = typename T::Type3D;
-
-	using TypeVector2DMapData = Vector2DMapData<ComponentType>;
-	using TypeVector3DMapData = Vector3DMapData<ComponentType>;
-
-	inline Vector4D() = default;
-	inline Vector4D(Vector4D const&) = default;
-	inline Vector4D(Vector4D &&) = default;
-	inline ~Vector4D() = default;
-
-	inline explicit Vector4D(ComponentType const value0, ComponentType const value1, 
-		ComponentType const value2, ComponentType const value3);
-
-	inline explicit Vector4D(ComponentType const value);
-	inline explicit Vector4D(TypeVector3D const& vector3D);
-
-	inline Vector4D& operator=(Vector4D const&) = default;
-	inline Vector4D& operator=(Vector4D &&) = default;
-
-	union
-	{
-		Vector1DComponent<T, 0> x;
-		Vector1DComponent<T, 1>	y;
-		Vector1DComponent<T, 2>	z;
-		Vector1DComponent<T, 3>	w;
-
-		Vector2DComponent<TypeVector2DMapData, 0, 1>	xy;
-		Vector2DComponent<TypeVector2DMapData, 0, 2>	xz;
-		Vector2DComponent<TypeVector2DMapData, 0, 3>	xw;
-
-		Vector2DComponent<TypeVector2DMapData, 1, 0>	yx;
-		Vector2DComponent<TypeVector2DMapData, 1, 2>	yz;
-		Vector2DComponent<TypeVector2DMapData, 1, 3>	yw;
-
-		Vector2DComponent<TypeVector2DMapData, 2, 0>	zx;
-		Vector2DComponent<TypeVector2DMapData, 2, 1>	zy;
-		Vector2DComponent<TypeVector2DMapData, 2, 3>	zw;
-
-		Vector2DComponent<TypeVector2DMapData, 3, 0>	wx;
-		Vector2DComponent<TypeVector2DMapData, 3, 1>	wy;
-		Vector2DComponent<TypeVector2DMapData, 3, 2>	wz;
-
-
-		Vector3DComponent<TypeVector3DMapData, 0, 1, 2>	xyz;
-		Vector3DComponent<TypeVector3DMapData, 0, 1, 3> xyw;
-
-		Vector3DComponent<TypeVector3DMapData, 0, 2, 1> xzy;
-		Vector3DComponent<TypeVector3DMapData, 0, 2, 3> xzw;
-
-		Vector3DComponent<TypeVector3DMapData, 1, 0, 2> yxz;
-		Vector3DComponent<TypeVector3DMapData, 1, 0, 3> yxw;
-
-		Vector3DComponent<TypeVector3DMapData, 1, 2, 0> yzx;
-		Vector3DComponent<TypeVector3DMapData, 1, 2, 3> yzw;
-
-		Vector3DComponent<TypeVector3DMapData, 2, 0, 1> zxy;
-		Vector3DComponent<TypeVector3DMapData, 2, 0, 3> zxw;
-
-		Vector3DComponent<TypeVector3DMapData, 2, 1, 0> zyx;
-		Vector3DComponent<TypeVector3DMapData, 2, 1, 3> zyw;
-
-		Vector4DComponent<T, 0, 1, 2, 3> xyzw;
-	};
+	T	data[CompCount];
 };
 
 
 template<class T>
-inline 
-Vector4D<T>::Vector4D(
-	ComponentType const value0, 
-	ComponentType const value1, 
-	ComponentType const value2, 
-	ComponentType const value3):
-	xyzw{ value0, value1, value2, value3 }
-{
-}
-
-
-template<class T>
-inline 
-Vector4D<T>::Vector4D(
-	ComponentType const value):
-	xyzw{ value, value, value, value }
-{
-}
-
-template<class T>
-inline 
-Vector4D<T>::Vector4D(
-	TypeVector3D const& vector3D):
-	xyz{ vector3D.xyz }
-{
-	w = static_cast<typename T::Type>(0);
-}
-
-
-template<class T>
-struct Vector4DMapData
+struct Vector<T, 4>
 {
 	using Type = T;
-	using Type2D = Vector2D<Vector2DMapData<T>>;
-	using Type3D = Vector3D<Vector3DMapData<T>>;
-	using Type4D = Vector4D<Vector4DMapData<T>>;
+	constexpr static CompCount = 4;
+
+	inline Vector() = default;
+	inline Vector(const Vector&) = default;
+	inline Vector(Vector&&) = default;
+	~Vector() = default;
+
+	inline explicit Vector(const T x, const T y, const T z, const T w);
+	inline explicit Vector(const T* srcData);
+
+	T	data[CompCount];
 };
 
-using Vector4DMapDataDec32 = Vector4DMapData<Decimal32>;
-using Vector4DMapDataDec64 = Vector4DMapData<Decimal64>;
-using Vector4DDec32 = Vector4D<Vector4DMapDataDec32>;
-using Vector4DDec64 = Vector4D<Vector4DMapDataDec64>;
+
+template<class T>
+inline 
+Vector<T, 2>::Vector(
+	const T x, 
+	const T y):
+	x{ x },
+	y{ y }
+{
+}
+
+template<class T>
+inline 
+Vector<T, 2>::Vector(
+	const T* srcData)
+{
+	std::memcpy(data, srcData, sizeof(T) * CompCount);
+}
+
+
+template<class T>
+inline 
+Vector<T, 3>::Vector(
+	const T x, 
+	const T y, 
+	const T z):
+	x{ x },
+	y{ y },
+	z{ z }
+{
+}
+
+
+template<class T>
+inline
+Vector<T, 3>::Vector(
+	const T* srcData)
+{
+	std::memcpy(data, srcData, sizeof(T) * CompCount);
+}
+
+
+template<class T>
+inline 
+Vector<T, 4>::Vector(
+	const T x, 
+	const T y, 
+	const T z, 
+	const T w):
+	x{ x },
+	y{ y },
+	z{ z },
+	w{ w }
+{
+}
+
+template<class T>
+inline
+Vector<T, 4>::Vector(
+	const T* srcData)
+{
+	std::memcpy(data, srcData, sizeof(T) * CompCount);
+}
 
 }
